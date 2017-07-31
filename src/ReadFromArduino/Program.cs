916aba928e8c96e80dfb9b5175af4d9aea757c62
Jeremy Lindsay <jeremy.lindsay@outlook.com>
2017-07-31 21:41:31 +0100

    Initial commit.

---
+++ src/ReadFromArduino/Program.cs
@@ -0,0 +1,21 @@
+﻿using ReadSerialInputFromUSB;
+using System;
+
+namespace ReadFromArduino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialInformation.GetPorts();
+
+            var serialInformation = new SerialInformation();
+
+            serialInformation.ReadFromPort();
+
+            Console.ReadKey();
+
+            serialInformation.SerialPort.Close();
+        }
+    }
+}
---
+++ src/ReadSerialInputFromUSB/SerialInformation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Ports;
+
+namespace ReadSerialInputFromUSB
+{
+    public class SerialInformation
+    {
+        public string OutputLine { get; set; }
+
+        public SerialPort SerialPort { get; set; }
+
+        public static void GetPorts()
+        {
+            Console.WriteLine("Serial ports available:");
+            Console.WriteLine("-----------------------");
+            foreach (var portName in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(portName);
+            }
+        }
+
+        public void ReadFromPort()
+        {
+            // Initialise the serial port on COM4.
+            // obviously we would normally parameterise this, but
+            // this is for demonstration purposes only.
+            this.SerialPort = new SerialPort("COM4")
+            {
+                BaudRate = 9600,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 8,
+                Handshake = Handshake.None
+            };
+
+            // Subscribe to the DataReceived event.
+            this.SerialPort.DataReceived += SerialPortDataReceived;
+
+            // Now open the port.
+            this.SerialPort.Open();
+        }
+
+        private void SerialPortDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var serialPort = (SerialPort)sender;
+
+            // Read the data that's in the serial buffer.
+            var serialdata = serialPort.ReadExisting();
+
+            // Write to debug output.
+            Debug.Write(serialdata);
+        }
+    }
+}
